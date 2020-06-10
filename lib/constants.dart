efc475b544af45607bfea0e85800f005cddef9c8
rishabh <rishabh20000jain@gmail.com>
2020-06-10 13:39:06 +0530

    .

--- lib/constants.dart
+++ lib/constants.dart
@@ -11,7 +11,7 @@ const IntroHeadingTextColorStyle = TextStyle(
 const PurpleColor = Color(0xFF444a5a);
 
 const String IntroTextForApp =
-    'This  App uses Edamam API services. It has several features like Recipe Search, Food Database, Nutrition Analysis. Soon, the web app will be powered with more new features like personalized recipe prediction based on your search and recommended healthy recipe. Currently we\'ve limited search result for every search due to API restriction usage, soon it\'ll be increased on.';
+    'This  App uses Edamam API services. It has several features like Recipe Search, Food Database, Nutrition Analysis. Soon, the app will be powered with more new features like personalized recipe prediction based on your search and recommended healthy recipe. Currently we\'ve limited search result for every search due to API restriction usage, soon it\'ll be increased on.';
 
 const String RecipeSearchText =
     'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s\, when an unknown printer took a galley of type and';
